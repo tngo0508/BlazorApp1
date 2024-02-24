1f134b39dea2451916af352df59e3005c16a0a3f
Thomas Ngo <tngo0508@gmail.com>
2024-02-24 14:09:42 -0800

    test

---
+++ BlazorApp1/Components/Pages/Counter.razor.cs
@@ -0,0 +1,11 @@
+namespace BlazorApp1.Components.Pages;
+
+public partial class Counter
+{
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}
